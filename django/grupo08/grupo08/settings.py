a77996e5a1bff729448867ef06c87f0ea3c53772
mijara <marjara35@gmail.com>
2017-12-04 22:57:55 -0300

    Actualizar settings

--- django/grupo08/grupo08/settings.py
+++ django/grupo08/grupo08/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -99,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
