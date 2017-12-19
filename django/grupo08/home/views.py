dfc03058c9b6dd13929ca1b24333cfb97974df48
Hernán Herreros Niño <hernan.herreros.13@sansano.usm.cl>
2017-12-19 01:33:05 -0300

    Show hostname in template

--- django/grupo08/home/views.py
+++ django/grupo08/home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+import socket
 
 
 # Create your views here.
 
 class HomePageView(View):
     def get(self, request):
-        return render(request, 'home/home.html')
+        return render(request, 'home/home.html', {
+            'hostname': socket.gethostname(),
+        })
