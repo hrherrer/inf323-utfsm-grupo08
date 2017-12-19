8955a0f99310848839263027d1febf6cdb414a4c
Hernán Herreros Niño <hernan.herreros.13@sansano.usm.cl>
2017-12-18 23:32:18 -0300

    Add Amazon s3 configuration

--- django/grupo08/grupo08/settings.py
+++ django/grupo08/grupo08/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'bbzeaaei8=dph@!xo)hw5!$vbxutwxpxqv)eq!w0qv*ft_kkis'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django.grupo08.mosorio.me']
+ALLOWED_HOSTS = ['django.grupo08.mosorio.me', 'localhost']
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -120,4 +122,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = 'grupo08.mosorio.me'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_SECURE_URLS = False
+
